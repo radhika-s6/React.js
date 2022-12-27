f801fb08cec7b5051fd08ad44fe35e622a0d4eb5
radhika <sikkaradhika313@gmail.com>
2022-12-27 09:53:35 +0530

    Link applied in file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Navbar(props)
 {
@@ -18,23 +19,23 @@ export default function Navbar(props)
 
 <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
-    <a className="navbar-brand" href = "/">Navbar</a>
+    <Link className="navbar-brand" to = "/">Navbar</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href = "/">Home</a>
+          <Link className="nav-link active" aria-current="page" to = "/">Home</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href = "/">About Us</a>
+          <Link className="nav-link" to = "/about">About Us</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">Feedback</a>
+          <Link className="nav-link" to="/form">Form</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href = "/">Contact Us</a>
+          <Link className="nav-link" to = "/">Contact Us</Link>
         </li>
       </ul>
 
