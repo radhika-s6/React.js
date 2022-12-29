79229411a013d660ba7b4f6c8d8ecd291d229197
radhika <sikkaradhika313@gmail.com>
2022-12-29 10:32:04 +0530

    form buttons disable setting done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 export default function Form(props) {
+    
     const [text, setText] = useState("");
     const onTextChange = (event) => {
         setText(event.target.value)
@@ -40,15 +41,15 @@ export default function Form(props) {
                     <textarea className="form-control" id="textarea" rows="10" onChange={onTextChange} style={{ backgroundColor: props.mode === "dark" ? "rgb(6, 75, 144)" : "white", color: props.mode === "dark" ? "white" : "black" }} value={text} placeholder="Enter your text" ></textarea>
                 </div>
 
-                <button className="btn btn-info mt-2" onClick={handleClickUp}>Uppercase</button>
-                <button className="btn btn-info mt-2 mx-2" onClick={handleClickLow}>Lowercase</button>
-                <button className="btn btn-info mt-2 mx-2" onClick={copyText}>Copy</button>
-                <button className="btn btn-info mt-2 mx-2" onClick={handleExtraSpace}>Remove Extra Space</button>
-                <button className="btn btn-info mt-2 mx-2" onClick={handleClear}>Clear</button>
+                <button disabled={text.length===0} className="btn btn-info mt-2" onClick={handleClickUp}>Uppercase</button>
+                <button disabled={text.length===0} className="btn btn-info mt-2 mx-2" onClick={handleClickLow}>Lowercase</button>
+                <button disabled={text.length===0} className="btn btn-info mt-2 mx-2" onClick={copyText}>Copy</button>
+                <button disabled={text.length===0} className="btn btn-info mt-2 mx-2" onClick={handleExtraSpace}>Remove Extra Space</button>
+                <button disabled={text.length===0} className="btn btn-info mt-2 mx-2" onClick={handleClear}>Clear</button>
                 </div>
                 <div className="container" style={{color:props.mode==="dark"?"White":"Black"}}>
                     <h2 className="mt-5">Text Summary: </h2>
-                    <p> {text.split(" ").filter((element)=>{return element.length!==0}).length} <b>Words</b> and {text.length} <b>Characters</b> </p>
+                    <p> {text.split(" ").filter((element)=>{return element.length!==0}).length} <b>Words</b> and {text.split("").filter((element)=>{return element.length!==0}).length} <b>Characters</b> </p>
                     <p> {0.08 * text.split(" ").length} Minutes to read </p>
                     <h2 className="mt-4">Preview: </h2>
                     <p>{text}</p>
